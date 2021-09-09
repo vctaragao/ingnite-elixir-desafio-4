4d00a456ae2652a681733390f4670f8a018572cf
Victor Moraes de Aragao <vct.aragao@gmail.com>
2021-09-08 22:39:42 -0300

    Setup Inicial

---
+++ lib/reserva_voos.ex
@@ -0,0 +1,5 @@
+defmodule ReservaVoos do
+  def hello do
+    :world
+  end
+end
---
+++ test/reserva_voos_test.exs
@@ -0,0 +1,8 @@
+defmodule ReservaVoosTest do
+  use ExUnit.Case
+  doctest ReservaVoos
+
+  test "greets the world" do
+    assert ReservaVoos.hello() == :world
+  end
+end

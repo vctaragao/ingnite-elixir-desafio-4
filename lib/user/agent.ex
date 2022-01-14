02f9ee225d2e71f1d1a09c7c5b2aed9578caf026
Victor Moraes de Aragõ <vct.aragao@gmail.com>
2022-01-13 22:26:50 -0300

    finalizar funcionalidade de salvar usuário

--- lib/user/agent.ex
+++ lib/user/agent.ex
@@ -8,4 +8,17 @@ defmodule FlightBooking.User.Agent do
   def save(%User{} = user), do: Agent.update(__MODULE__, &update_state(&1, user))
 
   defp update_state(state, %User{cpf: cpf} = user), do: Map.put(state, cpf, user)
+
+  def check_user(user_id) when is_bitstring(user_id) do
+    if get_by_user_id(user_id) do
+      {:ok, user_id}
+    else
+      {:error, user_id}
+    end
+  end
+
+  def get(cpf), do: Agent.get(__MODULE__, fn state -> Map.get(state, cpf) end)
+
+  defp get_by_user_id(user_id),
+    do: Agent.get(__MODULE__, fn state -> Map.get(state, :id, user_id) end)
 end

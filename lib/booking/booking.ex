fe158386a9c2bbdc2df195295816b43c8ac85131
Victor Moraes de Aragõ <vct.aragao@gmail.com>
2022-01-13 22:26:20 -0300

    Ajuste funcionalidade de reserva

--- lib/booking/booking.ex
+++ lib/booking/booking.ex
@@ -1,7 +1,14 @@
 defmodule FlightBooking.Booking.Booking do
-  @keys [:id, :complete_date, :local_origin, :local_destination, :user_id]
+  alias FlightBooking.Booking.Agent, as: AgentBooking
+
+  @keys [:id, :complete_date, :origin, :destination, :user_id]
 
   @enforce_keys @keys
 
   defstruct @enforce_keys
+
+  def save_or_update(%__MODULE__{} = booking) do
+    AgentBooking.save(booking)
+    {:ok, %{message: "Reserva salva ou atualizada", booking_id: booking.id}}
+  end
 end
